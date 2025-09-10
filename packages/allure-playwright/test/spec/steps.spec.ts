18bd23b9bab53e6e44368c83c29905416b882226
Anton Aftakhov <simple1elf@gmail.com>
2025-09-10 19:46:34 +0500

    change attach test 16 soft

--- packages/allure-playwright/test/spec/steps.spec.ts
+++ packages/allure-playwright/test/spec/steps.spec.ts
@@ -269,9 +269,9 @@ it("should attach attachments to correct steps in hooks and test steps", async (
   expect(beforeAllStep.steps).toHaveLength(1);
 
   const beforeAllAttachmentStep = beforeAllStep.steps[0];
-  expect(beforeAllAttachmentStep.name).toBe("test");
+  expect.soft(beforeAllAttachmentStep.name).toBe("test");
   expect(beforeAllAttachmentStep.attachments).toHaveLength(1);
-  expect(beforeAllAttachmentStep.attachments[0]).toEqual(
+  expect.soft(beforeAllAttachmentStep.attachments[0]).toEqual(
     expect.objectContaining({
       name: "test",
       type: "text/plain",
@@ -283,9 +283,9 @@ it("should attach attachments to correct steps in hooks and test steps", async (
   expect(beforeEachStep.steps).toHaveLength(1);
 
   const beforeEachAttachmentStep = beforeEachStep.steps[0];
-  expect(beforeEachAttachmentStep.name).toBe("test");
+  expect.soft(beforeEachAttachmentStep.name).toBe("test");
   expect(beforeEachAttachmentStep.attachments).toHaveLength(1);
-  expect(beforeEachAttachmentStep.attachments[0]).toEqual(
+  expect.soft(beforeEachAttachmentStep.attachments[0]).toEqual(
     expect.objectContaining({
       name: "test",
       type: "text/plain",
@@ -297,9 +297,9 @@ it("should attach attachments to correct steps in hooks and test steps", async (
   expect(test3Step.steps).toHaveLength(1);
 
   const test3AttachmentStep = test3Step.steps[0];
-  expect(test3AttachmentStep.name).toBe("test");
+  expect.soft(test3AttachmentStep.name).toBe("test");
   expect(test3AttachmentStep.attachments).toHaveLength(1);
-  expect(test3AttachmentStep.attachments[0]).toEqual(
+  expect.soft(test3AttachmentStep.attachments[0]).toEqual(
     expect.objectContaining({
       name: "test",
       type: "text/plain",
@@ -311,9 +311,9 @@ it("should attach attachments to correct steps in hooks and test steps", async (
   expect(test4Step.steps).toHaveLength(1);
 
   const test4AttachmentStep = test4Step.steps[0];
-  expect(test4AttachmentStep.name).toBe("test");
+  expect.soft(test4AttachmentStep.name).toBe("test");
   expect(test4AttachmentStep.attachments).toHaveLength(1);
-  expect(test4AttachmentStep.attachments[0]).toEqual(
+  expect.soft(test4AttachmentStep.attachments[0]).toEqual(
     expect.objectContaining({
       name: "test",
       type: "text/plain",
@@ -329,9 +329,9 @@ it("should attach attachments to correct steps in hooks and test steps", async (
   expect(afterEachStep.steps).toHaveLength(1);
 
   const afterEachAttachmentStep = afterEachStep.steps[0];
-  expect(afterEachAttachmentStep.name).toBe("test");
+  expect.soft(afterEachAttachmentStep.name).toBe("test");
   expect(afterEachAttachmentStep.attachments).toHaveLength(1);
-  expect(afterEachAttachmentStep.attachments[0]).toEqual(
+  expect.soft(afterEachAttachmentStep.attachments[0]).toEqual(
     expect.objectContaining({
       name: "test",
       type: "text/plain",
@@ -343,20 +343,33 @@ it("should attach attachments to correct steps in hooks and test steps", async (
   expect(afterAllStep.steps).toHaveLength(1);
 
   const afterAllAttachmentStep = afterAllStep.steps[0];
-  expect(afterAllAttachmentStep.name).toBe("test");
+  expect.soft(afterAllAttachmentStep.name).toBe("test");
   expect(afterAllAttachmentStep.attachments).toHaveLength(1);
-  expect(afterAllAttachmentStep.attachments[0]).toEqual(
+  expect.soft(afterAllAttachmentStep.attachments[0]).toEqual(
     expect.objectContaining({
       name: "test",
       type: "text/plain",
     }),
   );
 
-  // 0
+  const [attachment1] = beforeAllAttachmentStep.attachments;
+  const [attachment2] = beforeEachAttachmentStep.attachments;
   const [attachment3] = test3AttachmentStep.attachments;
-  // const [attachment4] = testResult.steps[2].steps[0].attachments;
-  // 4
+  const [attachment4] = test4AttachmentStep.attachments;
+  const [attachment5] = afterEachAttachmentStep.attachments;
+  const [attachment6] = afterAllAttachmentStep.attachments;
 
+  expect(attachments).toHaveProperty(attachment1.source);
+  expect(attachments).toHaveProperty(attachment2.source);
   expect(attachments).toHaveProperty(attachment3.source);
-  expect(Buffer.from(attachments[attachment3.source] as string, "base64").toString()).toEqual("test3");
+  expect(attachments).toHaveProperty(attachment4.source);
+  expect(attachments).toHaveProperty(attachment5.source);
+  expect(attachments).toHaveProperty(attachment6.source);
+
+  expect.soft(Buffer.from(attachments[attachment1.source] as string, "base64").toString()).toEqual("test1");
+  expect.soft(Buffer.from(attachments[attachment2.source] as string, "base64").toString()).toEqual("test2");
+  expect.soft(Buffer.from(attachments[attachment3.source] as string, "base64").toString()).toEqual("test3");
+  expect.soft(Buffer.from(attachments[attachment4.source] as string, "base64").toString()).toEqual("test4");
+  expect.soft(Buffer.from(attachments[attachment5.source] as string, "base64").toString()).toEqual("test5");
+  expect.soft(Buffer.from(attachments[attachment6.source] as string, "base64").toString()).toEqual("test6");
 });
